f03fcb46842f609cb121b549c8638bffc8a0f31d
Anelia Mechkarova <AMechkarova@nemetschek.bg>
2024-04-09 00:29:16 +0300

    test

---
+++ TestAutomationATPLevel2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAutomationATPLevel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
