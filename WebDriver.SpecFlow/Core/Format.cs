a860adedd252c1aed6e68fcb3d8a4bb243eea17c
Anelia Mechkarova <AMechkarova@nemetschek.bg>
2024-05-20 17:18:31 +0300

    SpecFlow demo exercise

---
+++ WebDriver.SpecFlow/Core/Format.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriver.SpecFlow.Core
+{
+    public enum Format
+    {
+        Fractions,
+        Decimal
+    }
+}
---
+++ WebDriver.SpecFlow/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebDriver.SpecFlow
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
